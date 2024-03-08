33a78f54f2807fd7adf028c2d03a2bd1fbef1d55
Taran-17 <85805491+Taran-17@users.noreply.github.com>
2024-03-08 20:52:12 +0530

    Update encoding.py

--- encoding.py
+++ encoding.py
@@ -1,1 +1,15 @@
-
+regs = {
+    "x0": "5'60000_0",
+    "xl": "5'60000-1",
+    "x2": "5'b0001-0",
+    "x3": "560001-1",
+    "x4": "5'6010_0",
+    "x5": "560010_1",
+    "x6-7": "5'600-{110,111}",
+    "x8": "",  # Assuming an empty string for the second register at this address
+    "x9": "5'60101_{0,1}",
+    "x10-11": "",  # Assuming an empty string for the second register at this address
+    "x12-17": "(5'6011-(00-11}),(561000.{0,1})",
+    "x18-27": "5'61-(0010-1011}",
+    "x28-31": "5'b111_{00-11}"
+}
